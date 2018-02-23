4f1926a1621e38abc2401a70c25ba32dff865ef0
Reto Zingg <reto.zingg@ge.com>
2018-02-23 14:02:12 +0200

    changes in lib

--- lib/seapigs/execution-filters.rb
+++ lib/seapigs/execution-filters.rb
@@ -2,13 +2,13 @@ require './config/environment.rb'
 
 class ExecutionFilters < Producer
 
-	@patterns = [ 'execution:filters' ]
+	@patterns = [ 'execution-filters' ]
 
 
-	def self.produce(object_id)
+	def self.produce(seapig_object_id)
 		version = SeapigDependency.versions('Execution')
 		creators = User.find_by_sql("SELECT DISTINCT users.id, nickname FROM users, executions WHERE users.id = executions.user_id ORDER BY users.nickname")
-		tags = Property.find_by_sql("SELECT DISTINCT p.name, v.value FROM properties p, values v, execution_values ev WHERE p.id = v.property_id AND ev.value_id = v.id AND p.id IN (SELECT p.id FROM properties p, execution_values ev, values v WHERE v.property_id = p.id AND ev.value_id = v.id GROUP BY p.id HAVING count(DISTINCT v.id) < 50) ORDER BY p.name, v.value")
+		tags = Property.find_by_sql("SELECT DISTINCT p.name, v.value FROM properties p, values v, execution_values ev WHERE p.id = v.property_id AND ev.value_id = v.id AND p.id IN (SELECT p.id FROM properties p, execution_values ev, values v WHERE v.property_id = p.id AND ev.value_id = v.id GROUP BY p.id HAVING count(DISTINCT v.id) < 100) ORDER BY p.name, v.value")
 		data = {
 			creators: creators.map { |creator| creator.nickname },
 			tags: tags.map { |tag| tag.name + ':' + tag.value }
--- lib/seapigs/task.rb
+++ lib/seapigs/task.rb
@@ -5,21 +5,26 @@ class TaskSingle < Producer
 	@patterns = [ 'task-*' ]
 
 
-	def self.produce(object_id)
-		object_id =~ /task-(\d+)/
+	def self.produce(seapig_object_id)
+		seapig_object_id =~ /task-(\d+)/
 		id = $1.to_i
-		version = SeapigDependency.versions('Task','TaskStatus')
+		version = SeapigDependency.versions('Task')
 		task = Task.find(id)
 		data = {
 			id: task.id,
 			description: task.description,
-			artifacts: task.artifacts.map { |artifact|
+			artifacts: task.artifacts.group_by { |artifact| artifact.name }.map { |name, artifact_versions|
+				artifact = artifact_versions.sort_by { |artifact| artifact.created_at }[-1]
 				{
 					id: artifact.id,
 					name: artifact.name,
 					mimetype: artifact.mimetype,
+					size: artifact.size,
+					external_url: artifact.external_url,
+					views: ArtifactView.views(artifact)
 				}
-			}
+			},
+			requirements: task.requirement.description
 		}
 		[data, version]
 	end
